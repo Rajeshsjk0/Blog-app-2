06c073223d1f95fffa0cd7ad33e462b731258837
Rakesh S <rajesh@replikatech.com>
2023-02-18 01:26:26 +0530

    UI: modified hero section

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -5,22 +5,13 @@ import { Hero } from "./components";
 const HomeScreen = () => {
   return (
     <Flex
-      display="flex"
-      flex={1}
       flexDirection="column"
       alignItems="center"
       justifyContent="center"
       padding={0}
-      gap={128}
-      // position="absolute"
       w={"calc(100% - 11.119%)"}
-      height="auto"
-      // minH={"100vh"}
-      // left="81px"
-      // right="80px"
       marginLeft="5.56%"
       marginRight="5.559%"
-      margin-top="129px"
       backgroundColor="blue"
     >
       <Hero />
